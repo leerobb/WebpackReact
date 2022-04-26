7ed193b7724510bbcbc47d9ab4cdf0cce02cea13
leerobb <76053808+leerobb@users.noreply.github.com>
2022-04-26 18:15:14 -0400

    Add project files.

---
+++ WebpackReact/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebpackReact.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
